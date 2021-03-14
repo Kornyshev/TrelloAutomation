bb97076ef310846906e38f77d89981475f13bdff
Evgeniy Kornyshev <Evgeniy_Kornyshev@epam.com>
2021-03-14 19:46:46 +0300

    Some tiny changes

---
+++ src/test/java/tests/TestNgRunner.java
@@ -0,0 +1,10 @@
+package tests;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {"src/test/resources/hw5"},
+        plugin = {"io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"})
+public class TestNgRunner extends AbstractTestNGCucumberTests {
+}
